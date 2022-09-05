17db86c4e11933e14e5ef89f4cd13c3ec099305d
JGibbings <j_gibbings@msn.com>
2022-09-05 19:17:51 -0400

    all 3 fetches now pull data properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,18 +26,79 @@ var cityToGeoPosit = function(city) {
             if (response.ok) {
                 response.json().then(function(data) {
                     var latLon = [data[0].lat, data[0].lon]
-                    console.log(latLon);
+                    getWeather(latLon);
                 });
             } else {
-                console.log("Error");
+                var errorEl = $("<p>").addClass("error-notice col-9 bg-danger p-2").text("Lat/Lon Error Code: " + data.cod);
+                $("#search-form").append(errorEl);
             }
         })
 };
 // passes lat/long to get weather obj
-var getWeather = function(GeoPosit) {};
+var getWeather = function(geoPosit) {
+    var apiUrlCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoPosit[0] + "&lon=" + geoPosit[1] + "&appid=48bd45b2787b6bc230706d9c63aab592";
+    var apiUrlFiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat=" + geoPosit[0] + "&lon=" + geoPosit[1] + "&appid=48bd45b2787b6bc230706d9c63aab592";
+    var fiveDayForecast = {};
+    fetch(apiUrlCurrentWeather)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data.name);
+                    // currentWeather = data;
+                    createWeatherDash(data);
+                });
+            } else {
+                var errorEl = $("<p>").addClass("error-notice col-9 bg-danger p-2").text("CurWea Error Code: " + data.cod);
+                $("#search-form").append(errorEl);
+            }
+        })
+    fetch(apiUrlFiveDay)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    fiveDayForecast = data;
+                });
+            } else {
+                var errorEl = $("<p>").addClass("error-notice col-9 bg-danger p-2").text("5Day Error Code: " + data.cod);
+                $("#search-form").append(errorEl);
+            }
+        })
+};
 
 // parses weather object into dynamic elements
-var createWeatherDash = function() {};
+var createWeatherDash = function(current) {
+//     <div class="border border-dark mr-3 p-3">
+//     <h2>Durham (9/3/2022) ☁️</h2>
+//     <ul>
+//         <li>
+//             <p>
+//                 Temp: 74.01 °F
+//             </p>
+//         </li>
+//         <li>
+//             <p>Wind: 8.22 MPH</p>
+//         </li>
+//         <li>
+//             <p>Humidity: 22%</p>
+//         </li>
+//         <li>
+//             <p>UV Index: <span>0.48</span></p>
+//         </li>
+//     </ul>
+// </div>
+    var weatherBox = $("<div>").addClass("border border-dark mr-3 p-3")
+    var weatherIcon = $("<img>").html("src='http:..openweathermap.org/img/w/" + current.icon + ".png' alt='Icon for current weather.'>");
+    var cityDateEl = $("<h2>").text(current.name + " (" + moment.unix(current.dt).format("M/D/YYYY") + ") ")
+    $("#right-column").append(cityDateEl, weatherIcon)
+
+
+
+
+
+};
+
+var createFiveDay = function(fiveDay) {};
 
 // on submit send city name to lat/long fetch
 $("#search-form").submit(function(event) {
@@ -54,6 +115,4 @@ $("#search-form").submit(function(event) {
 $("").on("click", "button", function() {});
 
 // loadSearchHistory();
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
